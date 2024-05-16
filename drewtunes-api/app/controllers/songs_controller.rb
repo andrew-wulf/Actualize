a8fc99585eda24daded662151bafc1c46e82337e
andrew-wulf <andrewdwulf@gmail.com>
2024-05-16 13:10:30 -0700

    finally figured out testing

--- drewtunes-api/app/controllers/songs_controller.rb
+++ drewtunes-api/app/controllers/songs_controller.rb
@@ -35,7 +35,14 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new()
-    @song.save
+    @song.update(title: params[:title] || @song.title,
+                artist: params[:artist] || @song.artist,
+                album: params[:album] || @song.album,
+                length: params[:length] || @song.length,
+                release_date: params[:release_date] || @song.release_date,
+                song_url: params[:song_url] || @song.song_url,
+                image_url: params[:image_url] || @song.image_url
+    )
 
     render(template: "songs/show", formats: :json)
   end
--- drewtunes-api/test/controllers/songs_controller_test.rb
+++ drewtunes-api/test/controllers/songs_controller_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class SongsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/songs.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Song.count, data.length
+  end
+
+  test "show" do
+    get "/songs/#{Song.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "artist", "album", "release_date", "length", "song_url", "image_url"], data.keys
+  end
+
+  test "update" do
+    song = Song.first
+    patch "/songs/#{song.id}.json", params: { length: 180}
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal 180, data["length"]
+  end
+
+  test "create" do
+    assert_difference "Song.count", 1 do
+      post "/songs/new.json", params: {title: "test", artist: 'mr. test', album: 'testing my patience'}
+    end
+  end
+
+  test "delete" do
+    assert_difference "Song.count", -1 do
+      delete "/songs/#{Song.last.id}.json"
+      assert_response 200
+    end
+  end
 end
+
+
+
